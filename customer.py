e5935e95cd349c05b3309f789928c0c8b3dbe346
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-05-20 06:53:31 +0300

    updated customer.py and coffee.py

--- customer.py
+++ customer.py
@@ -2,26 +2,25 @@ class Customer:
     def __init__(self, name):
         self.name = name
         self._orders = []
-    
+
     @property
     def name(self):
         return self._name
-    
+
     @name.setter
-    def name(self, new_name):
-        if not isinstance(new_name, str):
+    def name(self, value):
+        if not isinstance(value, str):
             raise TypeError("Name must be a string")
-        if not (1 <= len(new_name) <= 15):
-            raise ValueError("Name must be 1-15 characters")
-        self._name = new_name
+        if not (1 <= len(value) <= 15):
+            raise ValueError("Name must be between 1 and 15 characters")
+        self._name = value
 
     def orders(self):
         return self._orders.copy()
-    
+
     def coffees(self):
         return list({order.coffee for order in self._orders})
-    
+
     def create_order(self, coffee, price):
         from order import Order
-        new_order = Order(self, coffee, price)
-        return new_order
+        return Order(self, coffee, price)
