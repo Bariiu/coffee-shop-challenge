366c833e8cfccd4444cfa2b188a372623141fc80
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-05-20 06:54:17 +0300

    updated coffee.py

--- coffee.py
+++ coffee.py
@@ -1,33 +1,30 @@
 class Coffee:
     def __init__(self, name):
-        self.name = name 
+        self.name = name
         self._orders = []
-    
+
     @property
     def name(self):
         return self._name
-    
+
     @name.setter
-    def name(self, new_name):
-        if hasattr(self, "_name"):
-            raise AttributeError("Coffee names can't be changed")
-        if not isinstance(new_name, str):
-            raise TypeError("Name must bea string")
-        if len(new_name) < 3:
-            raise ValueError("Name must be more than or equal to 3 characters")
-        self._name = new_name
+    def name(self, value):
+        if hasattr(self, '_name'):
+            raise AttributeError("Coffee name cannot be changed")
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string")
+        if len(value) < 3:
+            raise ValueError("Coffee name must be ≥3 characters")
+        self._name = value
 
     def orders(self):
         return self._orders.copy()
-    
+
     def customers(self):
         return list({order.customer for order in self._orders})
-    
+
     def num_orders(self):
         return len(self._orders)
-    
+
     def average_price(self):
-        if not self._orders:
-            return 0
-        total = sum(order.price for order in self._orders)
-        return total / len(self._orders)
+        return sum(o.price for o in self._orders)/len(self._orders) if self._orders else 0
