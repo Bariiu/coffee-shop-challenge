e01c48fb04d4d210ab2af9e441eac9fb6a6c152c
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-05-20 07:07:41 +0300

    updated order.py

--- order.py
+++ order.py
@@ -7,3 +7,36 @@ class Order:
         customer._orders.append(self)
         coffee._orders.append(self)
 
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if hasattr(self, '_price'):
+            raise AttributeError("Price cannot be changed")
+        if not isinstance(value, float):
+            raise TypeError("Price must be a float")
+        if not (1.0 <= value <= 10.0):
+            raise ValueError("Price must be 1.0-10.0")
+        self._price = value
+
+    @property
+    def customer(self):
+        return self._customer
+
+    @customer.setter
+    def customer(self, value):
+        if not isinstance(value, Customer):
+            raise TypeError("Must be a Customer")
+        self._customer = value
+
+    @property
+    def coffee(self):
+        return self._coffee
+
+    @coffee.setter
+    def coffee(self, value):
+        if not isinstance(value, Coffee):
+            raise TypeError("Must be a Coffee")
+        self._coffee = value
